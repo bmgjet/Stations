5b3768eff0170ba173a5ca8672773d8edf3d53ad
bmgjet <50484759+bmgjet@users.noreply.github.com>
2021-07-09 18:49:14 +1200

    Version 1.0.2
    Adds support for Youtube2MP3 server
    Adds support to disable .mp3 check.

--- Stations.cs
+++ Stations.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("Stations", "bmgjet", "1.0.1")]
+    [Info("Stations", "bmgjet", "1.0.2")]
     class Stations : RustPlugin
     {
         #region Declarations
         const string perm = "Stations.use";
+        const string permyoutube = "Stations.youtube";
+        const string URL = "example.com:1234"; //Your YouTube2MP3 Server IP and Port
+        bool BypassMp3Check = false;
         private PropertyInfo _CurrentRadioIp = typeof(BoomBox).GetProperty("CurrentRadioIp");
         #endregion
 
@@ -15,14 +18,132 @@ namespace Oxide.Plugins
         void Init()
         {
             permission.RegisterPermission(perm, this);
+            permission.RegisterPermission(permyoutube, this);
         }
         #endregion
 
         public void noBB(BasePlayer player)
         {
-            player.ChatMessage("<color=red>Could'nt find boombox deployed or held.</color>");
+            player.ChatMessage("<color=red>Couldn't find boombox deployed or held.</color>");
         }
 
+        public bool CheckLink(string url)
+        {
+            if (!BypassMp3Check)
+            {
+                if (!url.Contains(".mp3"))
+                {
+                    return false;
+                }
+            }
+            if (!url.Contains("http"))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ChangeStationPortable(HeldBoomBox portableradio, string newlink, BasePlayer player)
+        {
+            //send link to MP3 Stream provided.
+            _CurrentRadioIp.SetValue(portableradio.BoxController, newlink);
+            //siwtch off old channel
+            portableradio.BoxController.ServerTogglePlay(false);
+            //use current selected mp3 stream.
+            portableradio.BoxController.baseEntity.ClientRPC<string>(null, "OnRadioIPChanged", portableradio.BoxController.CurrentRadioIp);
+            //start playing
+            portableradio.BoxController.ServerTogglePlay(true);
+            Puts(player.displayName + " Played: " + newlink);
+            player.ChatMessage("Please wait for music to download/encode!");
+        }
+
+        public void ChangeStationDeployed(DeployableBoomBox radio, string newlink, BasePlayer player)
+        {
+            //send link to MP3 Stream provided.
+            _CurrentRadioIp.SetValue(radio.BoxController, newlink);
+            //switch off old channel
+            radio.BoxController.ServerTogglePlay(false);
+            //use current selected mp3 stream.
+            radio.BoxController.baseEntity.ClientRPC<string>(null, "OnRadioIPChanged", radio.BoxController.CurrentRadioIp);
+            //play
+            radio.BoxController.ServerTogglePlay(true);
+            Puts(player.displayName + " Played: " + newlink);
+            player.ChatMessage("Please wait for music to download/encode!");
+        }
+
+        public BaseEntity FindBox(BasePlayer player)
+        {
+            RaycastHit rhit;
+            if (!Physics.Raycast(player.eyes.HeadRay(), out rhit))
+            {
+                noBB(player);
+                return null;
+            }
+            var heldEntity = rhit.GetEntity();
+            if (heldEntity == null)
+            {
+                noBB(player);
+                return null;
+            }
+            if (rhit.distance > 5f)
+            {
+                noBB(player);
+                return null;
+            }
+            if (heldEntity.ShortPrefabName.Contains("boombox"))
+            {
+                return heldEntity;
+            }
+            noBB(player);
+            return null;
+        }
+
+        [ChatCommand("youtube")]
+        void ItemChangeYoutube(BasePlayer player, string arg2, string[] args)
+        {
+            if (!player.IPlayer.HasPermission(permyoutube))
+            {
+                player.ChatMessage("<color=red>Dont have persmission to use this command!</color>");
+                return;
+            }
+            if (URL == "example.com:1234")
+            {
+                player.ChatMessage("<color=red>Plugin Youtube2MP3 server URL must be setup</color>");
+                return;
+            }
+            //check there is a url arg
+            if (args.Length != 1)
+            {
+                player.ChatMessage("InvalidArgs");
+                return;
+            }
+            if (!args[0].Contains("youtube"))
+            {
+                player.ChatMessage("<color=red>Must be a youtube link!</color>");
+                return;
+            }
+
+            if (player.IsHoldingEntity<HeldBoomBox>())
+            {
+                //Must be boombox portable
+                HeldBoomBox portableradio = player.GetActiveItem().GetHeldEntity() as HeldBoomBox;
+                if (portableradio == null)
+                {
+                    noBB(player);
+                    return;
+                }
+                ChangeStationPortable(portableradio, "http://" + URL + "/RUST:" + player.displayName + "?YT:" + args[0], player);
+                return;
+            }
+            //check for deployed boombox
+            DeployableBoomBox radio = FindBox(player) as DeployableBoomBox;
+            if (radio == null)
+            {
+                return;
+            }
+            ChangeStationDeployed(radio, "http://" + URL + "/RUST:" + player.displayName + "?YT:" + args[0], player);
+            return;
+        }
 
 
         [ChatCommand("station")]
@@ -40,76 +161,27 @@ namespace Oxide.Plugins
                 return;
             }
             //checks its a valid stream
-            if (!args[0].Contains(".mp3") || !args[0].Contains("http"))
+            if (!CheckLink(args[0]))
             {
                 player.ChatMessage("<color=red>Must be a link to a mp3!</color>");
                 return;
             }
-            try
+            if (player.IsHoldingEntity<HeldBoomBox>())
             {
-                if(player.IsHoldingEntity<HeldBoomBox>())
+                //Must be boombox portable
+                HeldBoomBox portableradio = player.GetActiveItem().GetHeldEntity() as HeldBoomBox;
+                if (portableradio == null)
                 {
-                    //Must be boombox portable
-                    HeldBoomBox portableradio = player.GetActiveItem().GetHeldEntity() as HeldBoomBox;
-                    if (portableradio == null)
-                    {
-                        noBB(player);
-                        return;
-                    }
-
-                    if (args.Length == 1)
-                    {
-                        //send link to MP3 Stream provided.
-                        _CurrentRadioIp.SetValue(portableradio.BoxController, args[0]);
-                        //portableradio.BoxController.CurrentRadioIp = args[1]; 
-                    }
-                    //siwtch off old channel
-                    portableradio.BoxController.ServerTogglePlay(false);
-                    //use current selected mp3 stream.
-                    portableradio.BoxController.baseEntity.ClientRPC<string>(null, "OnRadioIPChanged", portableradio.BoxController.CurrentRadioIp);
-                    //start playing
-                    portableradio.BoxController.ServerTogglePlay(true);
-                    //End of portable boombox
+                    noBB(player);
                     return;
                 }
-
-
-                    //check for deployed boombox
-                    RaycastHit rhit;
-                    if (!Physics.Raycast(player.eyes.HeadRay(), out rhit))
-                    {
-                    noBB(player);
-                    return;
-                    }
-                    var heldEntity = rhit.GetEntity();
-                    if (heldEntity == null)
-                    {
-                    noBB(player);
-                    return;
-                    }
-                    if (rhit.distance > 5f)
-                    {
-                    noBB(player);
-                    return;
-                    }
-                    if (heldEntity.ShortPrefabName.Contains("boombox"))
-                    {
-                        DeployableBoomBox radio = heldEntity as DeployableBoomBox;
-                        if (radio == null) return;
-                        //send link to MP3 Stream provided.
-                        _CurrentRadioIp.SetValue(radio.BoxController, args[0]);
-                        //switch off old channel
-                        radio.BoxController.ServerTogglePlay(false);
-                        //use current selected mp3 stream.
-                        radio.BoxController.baseEntity.ClientRPC<string>(null, "OnRadioIPChanged", radio.BoxController.CurrentRadioIp);
-                        //play
-                        radio.BoxController.ServerTogglePlay(true);
-                        return;
-                    }
+                ChangeStationPortable(portableradio, args[0], player);
+                return;
             }
-            catch {}
-            noBB(player);
+            //check for deployed boombox
+            DeployableBoomBox radio = FindBox(player) as DeployableBoomBox;
+            if (radio == null) return;
+            ChangeStationDeployed(radio, args[0], player);
         }
-
     }
 }
